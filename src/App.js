e49ef367ca38cac21b40fcc4db66364ce8ec40cb
Rashik Tuladhar <rashiktuladhar13@gmail.com>
2024-01-24 23:43:30 +0545

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { useEffect } from "react";
 import { messaging, db } from "./firebase";
 import { getToken } from "firebase/messaging";
-// import logo from "./logo.svg";
 import "./App.css";
 import { collection, setDoc, doc } from "firebase/firestore";
 import { v4 as uuidv4 } from "uuid";
 function App() {
-  async function requestNotificationPermission() {
+  
+  
+  async function sendToken() {
     
     const permission = await Notification.requestPermission();
     if (permission === "granted") {
@@ -39,6 +39,7 @@ function App() {
     }
   }
 
+ 
   async function requestPermission() {
     const permission = await Notification.requestPermission();
     if (permission === "granted") {
@@ -46,92 +47,38 @@ function App() {
       
       // Send this token  to server ( db)
     } else if (permission === "denied") {
-      alert("You denied for the notification");
-    }
-  }
-  useEffect(() => {
-    // Req user for notification permission
-    requestPermission();
-  }, []);
+      console.log("You denied for the notification");
+    }}
+  // const sendNotification = () => {
+  //   // Check if the browser supports notifications
+  //   if ("Notification" in window) {
+  //     // Check if the user has granted permission
+  //     if (Notification.permission === "granted") {
+  //       // Create and show the notification
+  //       const notification = new Notification("Hello, Notification!", {
+  //         body: "This is a sample notification.",
+  //       });
 
-  const sendNotification = () => {
-    // Check if the browser supports notifications
-    if ("Notification" in window) {
-      // Check if the user has granted permission
-      if (Notification.permission === "granted") {
-        // Create and show the notification
-        const notification = new Notification("Hello, Notification!", {
-          body: "This is a sample notification.",
-        });
-
-        // You can also handle click events on the notification
-        notification.onclick = function () {
-          console.log("Notification clicked!");
-        };
-      } else {
-        console.warn("Permission for notifications not granted");
-      }
-    } else {
-      console.warn("Notifications not supported in this browser");
-    }
-  };
+  //       // You can also handle click events on the notification
+  //       notification.onclick = function () {
+  //         console.log("Notification clicked!");
+  //       };
+  //     } else {
+  //       console.warn("Permission for notifications not granted");
+  //     }
+  //   } else {
+  //     console.warn("Notifications not supported in this browser");
+  //   }
+  // };
 
   return (
     <div>
-      <button onClick={requestNotificationPermission}>
+      <button onClick={requestPermission}>
         Request Notification Permission
       </button>
-      <button onClick={sendNotification}>Send Notification</button>
+      <button onClick={sendToken}>Send Token</button>
     </div>
   );
 }
 
 export default App;
-// import React, { useEffect } from 'react';
-
-// const App = () => {
-//   useEffect(() => {
-//     // Check if the browser supports notifications
-//     if ('Notification' in window) {
-//       Notification.requestPermission().then((permission) => {
-//         if (permission === 'granted') {
-//           console.log('Notification permission granted');
-//         } else {
-//           console.warn('Permission for notifications denied');
-//         }
-//       });
-//     } else {
-//       console.warn('Notifications not supported in this browser');
-//     }
-//   }, []);
-
-//   const sendNotification = () => {
-//     // Check if the browser supports notifications
-//     if ('Notification' in window) {
-//       // Check if the user has granted permission
-//       if (Notification.permission === 'granted') {
-//         // Create and show the notification
-//         const notification = new Notification('Hello, Notification!', {
-//           body: 'This is a sample notification.',
-//         });
-
-//         // You can also handle click events on the notification
-//         notification.onclick = function () {
-//           console.log('Notification clicked!');
-//         };
-//       } else {
-//         console.warn('Permission for notifications not granted');
-//       }
-//     } else {
-//       console.warn('Notifications not supported in this browser');
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <button onClick={sendNotification}>Send Notification to iOS</button>
-//     </div>
-//   );
-// };
-
-// export default App;
